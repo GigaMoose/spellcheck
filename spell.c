0d7adea574f3c11f41919d9977a454e6e3afc72f
Matt Saner <matthew.saner@nyu.edu>
2019-09-21 14:07:12 -0400

    add all the files from the prof

---
+++ spell.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "dictionary.h"
+
+int check_words(FILE *fp, hashmap_t hashtable[], char *misspelled[])
+{
+	printf("test1");
+}
+
+bool check_word(const char *word, hashmap_t hashtable[])
+{
+ 	printf("test2");
+}
+
+bool load_dictionary(const char *dictionary_file, hashmap_t hashtable[])
+{
+	printf("test3");
+}
+
